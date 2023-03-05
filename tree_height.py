77a304d4b97030f9e7629f5faae8fd99027b95bd
Alina Eksta <124583566+alinaeksta@users.noreply.github.com>
2023-03-05 20:43:11 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -6,77 +6,35 @@ import numpy as np
 
 
 def main():
-  # implement input form keyboard and from files
-  check_for_I = input().upper()
-
-  if check_for_I == "I":
-    n = int(input("Input n: "))
-    # a=int(input("Size of array:"))
-
-    parents = np.array(input("Element:").split())
-
-    # print(parents[1])
-    compute_height(n, parents)
-
-  # let user input file name to use, don't allow file names with letter a
-  # account for github input inprecision
-
-  elif check_for_I == "F":
-    file_name = input().strip()
-    if 'a' in file_name:
-      print("File name cannot contain letter 'a'!")
-      return
-    try:
-      with open('test/' + file_name, 'r') as f:
-        n = int(f.readline())
-        parents = list(map(int, f.readline().split()))
-        # print(compute_height(n, parents))
-    except FileNotFoundError:
-      print("File not found!")
-      return
-
+  letter = input()
+  if "I" in letter:
+    count = int(input())
+    main = list(map(int,input().split()))
+    print(compute_height(count, main))
+    
+  else if "F" in letter:
+    filename = input()
+    if "a" not in filename:
+      with open(str("test/"+filename), mode="r") as fails:
+        count = int(fails.readline())
+        main = list(map(int, fails.readline().split()))
+        print(compute_height(count, main))
+        
 
 def compute_height(n, parents):
-  # Write this function
-  # heights = np.array()
-  counter = 0
-  # max_height = 0
-  # Your code here
-  for i in range(n):
-    # node_height = 0
-    if parents[i] == "-1":
-      start = i
-      counter = counter + 1
-      break
-                 
-  for g in range (n):
-    val = start
-    if parents[g] == str(val):
-      val = g
-      counter = counter + 1
+    zeros = np.zeros(n)
+    def height(i):
+        if zeros[i] != 0:
+            return tree[i]
+        else if parents[i] == -1: 
+            zeros[i] = 1
+        else: 
+            zeros[i] = height(parents[i]) + 1
+        return zeros[i]
 
-      
-
-  
-
-  print(counter)
-
-  
-      
-   
-      #print("trr")
-    # parent = parents[i]
-    # while parent != - 1:
-    #     if heights[i]:
-    #         node_height += heights[i]
-    #         break
-    #     node_height += 1
-    #     parent = parents[parent]
-    # heights[i] = node_height
-    # max_height = max(max_height, node_height)
-
-  # return max_height
-
+    for i in range(n):
+        height(i)
+    return max(zeros)
 
 #     # input number of elements
 #     # input values in one variable, separate with space, split these values in an array
